88cf921e699cfc69a9afa7e2556cbd0af2c18802
lhalbritter-tu <leo.halbritter@hotmail.com>
2022-06-15 16:21:47 +0200

    Tank Model begin

--- Tank_Model/tank.py
+++ Tank_Model/tank.py
@@ -1,6 +1,7 @@
 import math
 
 G = 9.81
+M_TO_PIXELS = 100
 
 
 class Hole:
@@ -9,12 +10,17 @@ class Hole:
 
 
 class Tank:
-    def __init__(self, holes: list[Hole](), q: float, max_depth=1):
+    def __init__(self, holes: list[Hole](), q: float, max_depth=1, max_holes=50):
         self.holes: list[Hole]() = holes
+        self.hole_callback = self.holes + create_holes(max_holes - len(self.holes), self.holes[0].d)
         self.q: float = q
+        self.depth: float = max_depth
 
     def add_hole(self, hole: Hole):
-        self.holes.append(hole)
+        self.holes.append(self.hole_callback[0])
+
+    def pop_hole(self):
+        self.holes.pop()
 
     def rem_hole(self, i: int):
         self.holes.remove(i)
@@ -26,13 +32,20 @@ class Tank:
         d = self.holes[0]
         return len([h for h in self.holes if h.d != d]) == 0
 
+    def set_diameter(self, d):
+        for hole in self.holes:
+            hole.d = d
+
     def get_depth(self):
-        if not self.check_holes():
-            return -1
+        #if not self.check_holes():
+        #    return -1
         d_holes = self.holes[0].d / 100
         n_holes = len(self.holes)
 
         return (1 / (2 * G)) * (((4 * self.q) / (n_holes * math.pi * (d_holes ** 2))) ** 2)
+
+    def get_dimensions(self, x, y):
+        return [x, y, len(self.holes) * self.holes[0].d, self.depth * M_TO_PIXELS]
 
 
 def create_holes(n: int, d: float):
