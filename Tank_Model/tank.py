d7a361a46a2649925d5396f42eb830a62acc2ed8
lhalbritter-tu <leo.halbritter@hotmail.com>
2022-06-20 15:56:11 +0200

    Minor changes

--- Tank_Model/tank.py
+++ Tank_Model/tank.py
@@ -29,14 +29,17 @@ class Tank:
         self.holes.remove(self.holes.__index__(hole))
 
     def check_holes(self):
-        d = self.holes[0]
-        return len([h for h in self.holes if h.d != d]) == 0
+        d = self.holes[0].d
+        ch = [h for h in self.holes if h.d != d]
+        return len(ch) == 0
 
     def set_diameter(self, d):
         for hole in self.holes:
             hole.d = d
 
     def get_depth(self):
+        if not self.check_holes():
+            return -1
         d_holes = self.holes[0].d / 100
         n_holes = len(self.holes)
 
@@ -52,4 +55,4 @@ def create_holes(n: int, d: float):
 
 if __name__ == '__main__':
     t = Tank(create_holes(25, 2), 0.06)
-    print(t.get_depth())
+    print(t.check_holes())
